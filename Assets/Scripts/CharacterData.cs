0450166b0f63d09c0c591a5a25f37ba68c188871
Hansatang <portokarlos@gmail.com>
2023-04-11 15:31:56 +0200

    New character Fix

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Weapons;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Scriptable Objects/Characters")]
+public class CharacterData : ScriptableObject
+{
+    public string characterName;
+    public int characterHealth;
+    public float characterSpeed;
+    public Sprite characterSprite;
+    public GameObject characterObject;
+    public UpgradeData startingWeapon;
+}
