dda00f4f797faae9945db36742599c4f3dbcb95d
Hansatang <portokarlos@gmail.com>
2023-04-11 15:32:11 +0200

    Character selection for Main menu

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -4,6 +4,13 @@ namespace Managers
 {
     public class SceneManager : MonoBehaviour
     {
+
+        public void StartGame(CharacterData characterData)
+        {
+            SelectedCharacter.selectedCharacter = characterData;
+            UnityEngine.SceneManagement.SceneManager.LoadScene((int) Scenes.GameScene);
+        }
+
         public void Menu()
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene((int) Scenes.MainMenuScene);
