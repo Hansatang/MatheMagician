9b9f5e5326a725ec6ad7e24a5d2330769104c591
Hansatang <portokarlos@gmail.com>
2023-05-07 20:21:46 +0200

    Help now opens only once for player

--- Assets/Scripts/UI/HelpMenu.cs
+++ Assets/Scripts/UI/HelpMenu.cs
@@ -5,18 +5,8 @@ namespace UI
     /// <summary>
     ///    Class responsible for help canvas that is used showing instruction
     /// </summary>
-    public class HelpCanvas : MonoBehaviour
+    public class HelpMenu : MonoBehaviour
     {
-        public GameObject helpPanel;
-
-        void Start()
-        {
-            if (PlayerPrefs.GetInt("SawHelp") == 0)
-            {
-                helpPanel.SetActive(true);
-            }
-        }
-
         public void CloseHelp()
         {
             PlayerPrefs.SetInt("SawHelp", 1);
